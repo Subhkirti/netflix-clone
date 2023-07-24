12a3716b66b3215c7946018c392c1b68295d9bbc
Shubhkirti-9 <shubhkirti@sheroes.in>
2023-07-24 09:47:26 +0530

    changing the hero image

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ["surl.li", "assets.nflxext.com","image.tmdb.org",],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "surl.li",
+      "assets.nflxext.com",
+      "image.tmdb.org",
+      "assets.nflxext.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
